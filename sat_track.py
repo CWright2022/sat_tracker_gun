b809f1e3a8531cc79000e9639a9fca7f20a0a401
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2022-11-19 21:52:41 -0500

    formatting

--- sat_track.py
+++ sat_track.py
@@ -1,17 +1,19 @@
 '''
 SATELLITE TRACKING MODULE
 this module tracks satellites
+Cayden Wright 11/19/22
 '''
 
 from skyfield.api import EarthSatellite, load, wgs84
 
 ts = load.timescale()
 
-class Satellite:
-    __slots__=["__tle_file","__azimuth","__elevation","__sat_obj","__distance"]
 
-    def __init__(self,tle_file):
-        self.__tle_file=tle_file
+class Satellite:
+    __slots__ = ["__tle_file", "__azimuth", "__elevation", "__sat_obj", "__distance"]
+
+    def __init__(self, tle_file):
+        self.__tle_file = tle_file
         self.parse_tle()
 
     def parse_tle(self):
@@ -21,8 +23,7 @@ class Satellite:
             tle_2 = next(file)
         # create ts object
         satellite = EarthSatellite(tle_1, tle_2, name, ts)
-        self.__sat_obj=satellite
-
+        self.__sat_obj = satellite
 
     def recalculate(self, obs_lat, obs_long, datetime_obj):
         '''
@@ -31,28 +32,27 @@ class Satellite:
         '''
         # create observer location object thing
         observer_loc = wgs84.latlon(obs_lat, obs_long)
-        #create timescale thing
-        time=ts.from_datetime(datetime_obj)
+        # create timescale thing
+        time = ts.from_datetime(datetime_obj)
         # get difference between satellite and observer locations
         difference = self.__sat_obj - observer_loc
         # idk how this works
         topocentric = difference.at(time)
         # actually get altitude, azimuth, and distance
         elevation, azimuth, distance = topocentric.altaz()
-        #set values
-        self.__elevation=convert_to_dd(elevation)
-        self.__azimuth=convert_to_dd(azimuth)
-        self.__distance=distance
+        # set values
+        self.__elevation = convert_to_dd(elevation)
+        self.__azimuth = convert_to_dd(azimuth)
+        self.__distance = distance
 
     def azimuth(self):
         return self.__azimuth
-    
+
     def elevation(self):
         return self.__elevation
 
     def distance(self):
         return self.__distance
-    
 
 
 def convert_to_dd(angle):
@@ -68,10 +68,3 @@ def convert_to_dd(angle):
     else:
         decimal = deg + (mins/60) + secs/3600
     return decimal
-
-# track_me=Satellite("tle.txt")
-# current_time=datetime.datetime(2022,11,20,2,26,30,0,tzinfo=utc)
-# track_me.recalculate(43.083556,-77.674333,current_time)
-# print(track_me.azimuth())
-# print(track_me.elevation())
-# print(track_me.distance())
